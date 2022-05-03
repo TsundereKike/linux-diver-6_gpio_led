95f08bf3ab3146887fb3b359a207262015f06a5b
TsundereKike <2682367495@qq.com>
2022-05-03 22:57:33 +0800

    release

---
+++ .gpio_led.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.ko := arm-linux-gnueabihf-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.ko /home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.o /home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.mod.o
---
+++ .tmp_versions/gpio_led.mod
@@ -0,0 +1,2 @@
+/home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.ko
+/home/tmh/linux/IMX6ULL/Linux_Drivers/6_gpio_led/gpio_led.o
